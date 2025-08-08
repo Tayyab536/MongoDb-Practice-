f48500f540753ea2cdf87a4e6cd0e43136cc48d7
Tayyab536 <mtayyabali536@gmail.com>
2025-08-08 16:32:06 +0500

    Insertion

--- Practice.js
+++ Practice.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
+import User from "./models/user.model.js";
 (async () => {
   try {
-    
     await mongoose.connect(
       "mongodb+srv://taibicode:v11MFg5hXd4yiz3x@mysql.efabav5.mongodb.net/DbPractice"
     );
@@ -11,3 +11,13 @@ import mongoose from "mongoose";
   }
 })();
 
+const insertOne = async () => {
+  const user = await User.insertOne({
+    name: "Tina Morris",
+    age: 29,
+    skills: ["Cloud Computing", "GCP"],
+    address: { city: "Minneapolis", zip: 55401 },
+    joined: new Date("2021-01-01"),
+    active: true,
+  });
+};
---
+++ models/user.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, minlength: 3 },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 6 },
+    age: { type: Number, min: 0, max: 120 },
+    isActive: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
